ceca66dc5a6d1001c65e66535a987f7ab17a8d88
gracielacesareo <graciela.cesareoc@gmail.com>
2024-10-19 21:37:18 -0600

    Add local storage. Load items from storage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Appointment } from '../models/appointment';
 
 @Component({
@@ -6,31 +6,51 @@ import { Appointment } from '../models/appointment';
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.scss']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
   public newAppointmentTitle: string = '';
   public newAppointmentDate: Date = new Date();
 
   public appointments: Appointment[] = []
 
+  ngOnInit() {
+     this.loadAppointments();
+  }
+
   public addNewAppointment(): void {
     if(this.newAppointmentTitle.length && this.newAppointmentDate) {
 
+      // Model new item
       let newAppointment = {
         id: Date.now(),
         title: this.newAppointmentTitle,
         date: this.newAppointmentDate
       }
 
+       // Add item to array
       this.appointments.push(newAppointment);
-      console.log(this.appointments)
+      localStorage.setItem('appointments', JSON.stringify(this.appointments))
       this.resetForm();
 
     }
   }
 
+  // Reset form
   public resetForm(): void {
     this.newAppointmentTitle = '';
     this.newAppointmentDate = new Date();
+  }
+
+  // Delete item from array
+  public deleteAppointment(index: number): void  {
+    this.appointments.slice(index, 1)
+    localStorage.setItem('appointments', JSON.stringify(this.appointments))
+  }
+
+  // Load items array
+  public loadAppointments() {
+    let savedAppointments = localStorage.getItem('appointments')
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : []
+    return this.appointments
   }
 
 }
